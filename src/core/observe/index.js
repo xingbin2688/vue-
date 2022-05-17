e55d5c1e46e9107dd439bb40d10085ab5eae0a3a
xingbin <xingbinhaihao@163.com>
2022-05-17 17:05:30 +0800

    递归侦测所有的key

--- src/core/observe/index.js
+++ src/core/observe/index.js
@@ -3,6 +3,9 @@ import Dep from './dep'
 // Object.defineProperty可以侦测到对象的变化
 // 每当从get的key中读取数据时，get函数便会被触发；每当往data的key中设置数据时，set函数触发
 function defineReactive(data, key, val) {
+    if (typeof val === 'object') {
+        new Observer(val)
+    }
     let dep = new Dep // 依赖
     Object.defineProperty(data, key, {
         enumerable: true, // 可枚举
@@ -20,4 +23,25 @@ function defineReactive(data, key, val) {
             dep.notify()
         }
     })
+}
+
+// 递归侦测 所有key
+export class Observer {
+    constructor(value) {
+        this.value = value
+        if (!Array.isArray(value)) {
+            this.walk(value)
+        }
+    }
+
+    /**
+     * walk会将每一个属性都转换成getter/setter的形式来侦测变化
+     * 这个方法只有在数据类型时Object时被调用
+     * */
+    walk(obj) {
+        const keys = Object.keys(obj)
+        for (let i = 0; i < keys.length; i++) {
+            defineReactive(obj, keys[i], obj[keys[i]])
+        }
+    }
 }
